78ede69e359d850f8f94914edd1790aba051fe54
chriscao1251 <chris.cao@funimation.com>
2019-05-16 14:40:02 -0500

    Add project files.

---
+++ Directory_DotNetCore_Web_API/Models/EmployeeModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Directory_DotNetCore_Web_API.Models
+{
+    public class EmployeeModel
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+
+        [BsonElement("firstName")]
+        public string firstName { get; set; }
+
+        [BsonElement("lastName")]
+        public string lastName { get; set; }
+
+        [BsonElement("address")]
+        public string address { get; set; }
+
+        [BsonElement("phoneNumbers")]
+        public List<int> phoneNumbers { get; set; }
+
+        [BsonElement("ActiveStatus")]
+        public bool activeStatus { get; set; } = false;
+
+        [BsonDateTimeOptions]
+        public DateTime? startDate { get; set; }
+
+        [BsonDateTimeOptions]
+        public DateTime? endDate { get; set; }
+
+    }
+}
---
+++ Directory_DotNetCore_Web_API/Services/DirectoryService.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+using Directory_DotNetCore_Web_API.Models;
+using Microsoft.Extensions.Configuration;
+
+namespace Directory_DotNetCore_Web_API.Services
+{
+    public class DirectoryService
+    {
+        private readonly IMongoCollection<EmployeeModel> _employees;
+
+        public DirectoryService(IConfiguration config)
+        {
+            var client = new MongoClient(config.GetConnectionString("DirectoryDb"));
+            var database = client.GetDatabase("DirectoryDb");
+            _employees = database.GetCollection<EmployeeModel>("employees");
+        }
+
+        public List<EmployeeModel> Get()
+        {
+            return _employees.Find(EmployeeModel => true).ToList();
+        }
+
+        public EmployeeModel Get(string id)
+        {
+            return _employees.Find<EmployeeModel>(employee => employee.Id == id).FirstOrDefault();
+        }
+
+        public EmployeeModel Create(EmployeeModel employee)
+        {
+            _employees.InsertOne(employee);
+            return employee;
+        }
+
+        public void Update(string id, EmployeeModel employeeIn)
+        {
+            _employees.ReplaceOne(employee => employee.Id == id, employeeIn);
+        }
+
+        public void Remove(EmployeeModel employeeIn)
+        {
+            _employees.DeleteOne(employee=> employee.Id == employeeIn.Id);
+        }
+
+        public void Remove(string id)
+        {
+            _employees.DeleteOne(employee => employee.Id == id);
+        }
+
+    }
+}
